a7a56bb660e20c295653705e418c28d6e8526811
Hidenari Nozaki <hidenari@gmail.com>
2016-05-27 21:10:32 +1200

    Change how to start ping pong

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var csp = require('js-csp');
 
 function* player(name, table, done) {
+  if (name === 'pong') {
+    // serve
+    console.log('serve from pong');
+    yield csp.put(table, {hits: 0});
+  }
   while (true) {
     var ball = yield csp.take(table);
     if (ball === csp.CLOSED) {
@@ -28,8 +33,6 @@ csp.go(function* () {
 
   csp.go(player, ["ping", table, done]);
   csp.go(player, ["pong", table, done]);
-
-  yield csp.put(table, {hits: 0});
 
   yield csp.take(done);
   console.log('game over');
